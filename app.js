4f7930cfc9a6260f4bf618d3255f0bad42f225fe
Greg Pfaff <greg.pfaff@razorfish.com>
2017-01-19 11:11:21 -0600

    adding WIP dice game

---
+++ app.js
@@ -0,0 +1,108 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+var resetGame =  document.querySelector('.btn-new');
+var holdButton = document.querySelector('.btn-hold');
+var playerOne = document.querySelector('.player-0-panel');
+var playerTwo = document.querySelector('.player-1-panel');
+var playerOneScore = document.getElementById('current-0');
+var playerTwoScore = document.getElementById('current-1');
+var playerOneNum = Number(playerOneScore.innerHTML);
+var playerTwoNum = Number(playerTwoScore.innerHTML);
+var dice = document.getElementsByTagName('img');
+var diceOne = document.getElementsByTagName('img')[0];
+var diceTwo = document.getElementsByTagName('img')[1];
+var diceThree = document.getElementsByTagName('img')[2];
+var diceFour = document.getElementsByTagName('img')[3];
+var diceFive = document.getElementsByTagName('img')[4];
+var diceSix = document.getElementsByTagName('img')[5];
+
+// Toggles Players
+
+function changePlayers() {
+  playerOne.classList.toggle('active');
+  playerTwo.classList.toggle('active');
+}
+
+// Resets Game
+function startNewGame() {
+  location.reload(resetGame);
+}
+
+function winGame() {
+  // if total gets to 100 end game
+
+}
+// Generates Random Dice Roll
+function rollDice(min, max) {
+  var min = Math.ceil(1);
+  var max = Math.floor(6);
+  var roll = Math.floor(Math.random() * (max - min + 1)) + min;
+  showDice(roll);
+  tabulateScore(roll);
+}
+
+// Calculate Player Points
+
+function tabulateScore(roll) {
+  console.log(roll);
+  console.log("tabscores")
+
+  var total = 0;
+  var newTotal = playerOneNum += roll;
+
+  console.log(newTotal);
+ 
+  if (playerOne.classList.contains('active')) {
+    if (roll === 1) {
+      changePlayers();
+      playerOneScore.textContent = 0;
+    } else {
+        return playerOneScore.textContent = newTotal;
+      }
+  } else {
+      return playerTwoScore.textContent = newTotal;
+    if (roll === 1) {
+      changePlayers();
+      playerTwoScore.textContent = 0;
+    }
+  }
+}
+
+// Show Dice Roll 
+function showDice(roll) {
+  for (var i = 0; i < dice.length; i++) {
+    dice[i].classList.remove('active');
+    switch(roll) {
+      case 1:
+      diceOne.classList.add('active');
+      break;
+      case 2:
+      diceTwo.classList.add('active');
+      break;
+      case 3:
+      diceThree.classList.add('active');
+      break;
+      case 4:
+      diceFour.classList.add('active');
+      break;
+      case 5:
+      diceFive.classList.add('active');
+      break;
+      case 6:
+      diceSix.classList.add('active');
+      break;
+      default: 
+      dice.classList.remove('active');
+      break;
+    }
+  }
+}
